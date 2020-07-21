60bd8f1b3f41d300a87175e0f6769a94307d6b56
Sierra Wallace <sskwallace@gmail.com>
2020-07-21 23:08:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,13 @@
 def valid_move?(board, index)
-  if position_taken?(board, index) == index.between?(0,8)
-    true
-    elsif position_taken?(board, index) == index.between?(0,8)
+  if position_taken?(board, index) == "X" || "O"
     false
-    else
-      false
-    end
+    elsif position_taken?(board, index) == " "
+    true
+  else
+    false
   end
+end
+
   
   
 
